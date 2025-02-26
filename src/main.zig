6950c6d81aeaae36b0f6724cabba0fe7717d32d7
spooky-th-ghost <colin@wildheartinteractive.com>
2025-02-25 21:52:51 -0500

    stuck on the render texture

--- src/main.zig
+++ src/main.zig
@@ -4,19 +4,25 @@ const Player = @import("player.zig").Player;
 const World = @import("core.zig").World;
 
 pub fn main() !void {
-    const screen_width = 1600;
-    const screen_height = 900;
+    var WINDOW_WIDTH: i32 = 1600;
+    var WINDOW_HEIGHT: i32 = 900;
+    const RENDER_WIDTH: i32 = 240;
+    const RENDER_HEIGHT: i32 = 160;
 
     rl.setConfigFlags(.{ .window_resizable = true });
 
-    // try physics.particle_sim();
-
-    rl.initWindow(screen_width, screen_height, "search action");
+    rl.initWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "search action");
+    const monitor = rl.getCurrentMonitor();
+    WINDOW_WIDTH = rl.getMonitorWidth(monitor);
+    WINDOW_HEIGHT = rl.getMonitorHeight(monitor);
     defer rl.closeWindow();
 
     const world = World.init();
 
     var player = Player.init(.{ .x = 0, .y = 2, .z = 0 });
+
+    var rt = try rl.loadRenderTexture(RENDER_WIDTH, RENDER_HEIGHT);
+    var scene = try rl.loadRenderTexture(RENDER_WIDTH, RENDER_HEIGHT);
 
     while (!rl.windowShouldClose()) {
         var velocity = rl.Vector3.zero();
@@ -36,16 +42,35 @@ pub fn main() !void {
             const transformed_velocity = world.interpolate_vector(velocity);
             player.move(transformed_velocity);
         }
-        rl.beginDrawing();
-        rl.clearBackground(rl.Color.ray_white);
 
         world.start_3d();
         defer world.end_3d();
         {
-            rl.drawGrid(10, 1);
-            player.draw();
-        }
+            {
+                scene.begin();
+                rl.clearBackground(rl.Color.white);
+                rl.drawGrid(10, 1);
+                player.draw();
+                scene.end();
+            }
 
-        rl.endDrawing();
+            rt.begin();
+            scene.texture.draw(0, 0, rl.Color.white);
+            rt.end();
+
+            rl.beginDrawing();
+            rl.drawTexturePro(rt.texture, .{
+                .x = 0,
+                .y = 0,
+                .width = RENDER_WIDTH,
+                .height = RENDER_HEIGHT,
+            }, .{
+                .x = 0,
+                .y = 0,
+                .width = @floatFromInt(WINDOW_WIDTH),
+                .height = @floatFromInt(WINDOW_HEIGHT),
+            }, rl.Vector2.zero(), 0, rl.Color.white);
+            rl.endDrawing();
+        }
     }
 }
